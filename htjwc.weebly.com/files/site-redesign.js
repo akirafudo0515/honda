a3ce7a4bc8e99480da6633aef7b08b8bd4f6075e
akirafudo0515 <akirafudo0515@gmail.com>
2026-08-14 19:31:48 +0800

    Use the original HTJWC logo file without editing the letters.
    Keep the provided artwork intact in the header and favicon.

--- htjwc.weebly.com/files/site-redesign.js
+++ htjwc.weebly.com/files/site-redesign.js
@@ -57,7 +57,7 @@
     header.innerHTML =
       '<div class="honda-header-inner">' +
       '<a class="honda-brand" href="index.html">' +
-      '<img class="honda-brand-logo" src="files/htjwc-logo.png" alt="HTJWC" />' +
+      '<img class="honda-brand-logo" src="files/htjwc-logo.png?v=20260814d" alt="HTJWC" />' +
       '<span class="honda-brand-text">' +
       '<strong>台灣本田聯合福委會</strong>' +
       '<span>特約商店公佈欄</span>' +
